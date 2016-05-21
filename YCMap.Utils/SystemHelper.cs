786bf54095b49f52658bd308ab9e4e29dcce5294
YinChaoOnline <ychaowhu@foxmail.com>
2016-05-21 17:20:08 +0800

    添加转换单位函数

---
+++ YCMap.Utils/SystemHelper.cs
@@ -0,0 +1,21 @@
+﻿using ESRI.ArcGIS.esriSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YCMap.Utils
+{
+   public class SystemHelper
+    {
+        public static String ConvertEsriUnit(esriUnits units)
+        {
+            String zhUnits = "unknown";
+            if (units.ToString().Contains("esri"))
+            {
+                zhUnits = units.ToString().Substring(4);
+            }
+            return zhUnits;
+        }
+    }
+}
