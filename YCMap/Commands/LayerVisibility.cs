94b78d748f7165e193007d2240abf74e98dd8ce1
YinChaoOnline <ychaowhu@foxmail.com>
2016-05-21 17:19:17 +0800

    添加layervisibility,removelayer,zoomtolayer 三个command

---
+++ YCMap/Commands/LayerVisibility.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.ADF.BaseClasses;
+
+namespace YCMap.Commands
+{
+    class LayerVisibility : BaseCommand, ICommandSubType
+    {
+        private IHookHelper m_hookHelper = new HookHelperClass();
+        private Int32 m_subType;
+
+        public LayerVisibility()
+        {
+
+        }
+
+        public override void OnCreate(object hook)
+        {
+            m_hookHelper.Hook = hook;
+        }
+
+        public override void OnClick()
+        {
+            for (int i = 0; i <= m_hookHelper.FocusMap.LayerCount - 1; i++)
+            {
+                if (m_subType == 1) m_hookHelper.FocusMap.get_Layer(i).Visible = true;
+                if (m_subType == 2) m_hookHelper.FocusMap.get_Layer(i).Visible = false;
+            }
+            m_hookHelper.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeography, null, null);
+        }
+
+        public override string Caption
+        {
+            get
+            {
+                if (m_subType == 1) return "打开所有图层";
+                else return "关闭所有图层";
+            }
+        }
+        public override bool Enabled
+        {
+            get
+            {
+                bool enabled = false;
+                int i;
+                if (m_subType == 1)
+                {
+                    for (i = 0; i <= m_hookHelper.FocusMap.LayerCount - 1; i++)
+                    {
+                        if (m_hookHelper.ActiveView.FocusMap.get_Layer(i).Visible == false)
+                        {
+                            enabled = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (i = 0; i <= m_hookHelper.FocusMap.LayerCount - 1; i++)
+                    {
+                        if (m_hookHelper.ActiveView.FocusMap.get_Layer(i).Visible == true)
+                        {
+                            enabled = true;
+                            break;
+                        }
+                    }
+                }
+                return enabled;
+            }
+        }
+
+
+        public int GetCount()
+        {
+            return 2;
+        }
+
+        public void SetSubType(int SubType)
+        {
+            m_subType = SubType;
+        }
+
+    }
+}
---
+++ YCMap/Commands/ZoomToLayer.cs
@@ -0,0 +1,30 @@
+﻿using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YCMap.Commands
+{
+    public sealed class ZoomToLayer : BaseCommand
+    {
+        private IMapControlDefault m_mapControl;
+        public ZoomToLayer()
+        {
+            base.m_caption = "放大到图层";
+        }
+
+        public override void OnCreate(object hook)
+        {
+            m_mapControl = hook as IMapControlDefault;
+        }
+
+        public override void OnClick()
+        {
+            ILayer layer = m_mapControl.CustomProperty as ILayer;
+            m_mapControl.Extent = layer.AreaOfInterest;
+        }
+    }
+}
